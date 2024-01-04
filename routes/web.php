8dd8b7d5543f1a678d1c95a0def88844b75440eb
aussenseiter13 <Carefully5693@proton.me>
2024-01-04 16:28:08 +0100

    Finished episode 10

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('post/{post}', function($slug){
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(!file_exists($path)){
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{$slug}", 5, function() use ($path){
+        var_dump('file_get_conents');
+        return file_get_contents($path);
+    });
+
+
     return view('post', [
-        'post' => file_get_contents(__DIR__ . '/../resources/posts/' . $slug . '.html')
+        'post' => $post
     ]);
 })->where('post', '[A-z_\-]+');
 
