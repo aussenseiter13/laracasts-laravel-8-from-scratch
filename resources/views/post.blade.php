4d4a60d105f5371f2d22089fd8d2dbc3aa5a745b
aussenseiter13 <Carefully5693@proton.me>
2024-01-05 11:47:07 +0100

    Finished episode 14: Blade The Absolute Basics

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,8 +9,8 @@
 
 <body>
     <article>
-        <h1><?= $post->title ?></h1>
-        <p><?= $post->body ?></p>
+        <h1>{{ $post->title }}</h1>
+        <p>{{ $post->body }}</p>
     </article>
     <a href="/">Return Home</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,18 +9,18 @@
 
 <body>
     <main>
-        <?php foreach($posts as $post) : ?>
+        @foreach($posts as $post)
         <article>
             <h1>
-                <a href="/posts/<?= $post->slug ?>">
-                    <?= $post->title ?>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
             <div>
-                <?= $post->excerpt ?>
+                {{ $post->excerpt }}
             </div>
         </article>
-        <?php endforeach; ?>
+        @endforeach
     </main>
 </body>
 
